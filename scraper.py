b2b590e4c9010ba307fa1f350fedd18b0068a7ee
Niles Rogoff <nilesrogoff@gmail.com>
2016-07-29 19:28:39 -0700

    Added status

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 import urllib.request, json, BTEdb, re, time
 db = BTEdb.Database("/dev/shm/lainchan-scraper.json")
 db.BeginTransaction(False)
+status = open("/dev/shm/lainchan-scraper-status", "w")
+status.write("Update in progress")
+status.close()
 boards = ['lam', "tech"]
 regex = re.compile(r"\b[A-Za-z0-9]{46}\b")
 if db.TableExists("table"):
@@ -8,6 +11,10 @@ if db.TableExists("table"):
 else:
     db.CreateTable("table")
 for board in boards:
+    # DEBUG
+    # time.sleep(5)
+    # continue
+    # END DEBUG
     threads = json.loads(urllib.request.urlopen("https://lainchan.org/"+board+"/threads.json").read().decode("utf-8"))
     for page in threads:
         for thread in page["threads"]:
@@ -27,3 +34,7 @@ for board in boards:
                             i+= 1
 db.CommitTransaction()
 db.Destroy()
+import time
+status = open("/dev/shm/lainchan-scraper-status", "w")
+status.write("Last scrape at <span class=\"date\">" + str(int(time.time())) + "</span><br /> Next scrape at <span class=\"date\">" + str(3600 - (int(time.time()) % 3600) + int(time.time())) + "</span>")
+status.close()
